c67f6f0edd9bb15bf33e9a13d72606a5d480e504
rosenfeldjosh <jirosenf@uci.edu>
2016-12-14 12:51:14 +0100

    Removed build folder

--- build/assimp/build/contrib/zlib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/admin/Documents/Visual Studio 2015/Graphics project/build/assimp/src/assimp/contrib/zlib
-# Build directory: C:/Users/admin/Documents/Visual Studio 2015/Graphics project/build/assimp/build/contrib/zlib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/glm/src/glm-build/glm/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/admin/Documents/Visual Studio 2015/Graphics project/build/glm/src/glm/glm
-# Build directory: C:/Users/admin/Documents/Visual Studio 2015/Graphics project/build/glm/src/glm-build/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
